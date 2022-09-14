583993a720f91b53ea2765c0019e4cab9aad1d06
JunaidSikander <junaid_sikander@live.com>
2022-09-14 23:51:50 +0500

    [ADD] Pool address route in server.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import {notFound} from "./middlewares/notFound.js";
 import disclaimerRoutes from "./routes/disclaimer.js";
 import Errors from "./middlewares/errors.js";
+import poolAddressRoutes from "./routes/poolAddreses.js";
 
 
 dotenv.config();
@@ -16,7 +17,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // Routes
-app.use('/api', disclaimerRoutes)
+app.use('/api/disclaimer', disclaimerRoutes)
+app.use('/api/poolAddress', poolAddressRoutes);
 
 // Middlewares
 app.use(notFound);
